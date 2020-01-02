ffe5ae4b52234ecad3141951a54afcfd5c94cb11
Numan Hussain <ahnuman5@gmail.com>
2020-01-02 22:42:56 +0600

    added important few functions

--- functions.php
+++ functions.php
@@ -4,7 +4,9 @@
 
      load_theme_textdomain("wp-news"); 
      add_theme_support("post-thumbnails"); 
-     add_theme_support("title-tag");                                     
+     add_theme_support("title-tag");                   
+     add_theme_support("custom-background");      
+     add_theme_support( "custom-header" );            
 
  
 
